0c2b914cc3501681be27e669ad20d465cf9b36ea
FaistDev <ben@faistdev.at>
2019-01-11 12:11:31 +0100

    MainGui change

--- Easy_Accountancy_Software/src/MainGUI.java
+++ Easy_Accountancy_Software/src/MainGUI.java
@@ -26,21 +26,41 @@ public class MainGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btAnlagenVz = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btAnlagenVz.setText("Anlagenverzeichnis");
+        btAnlagenVz.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btAnlagenVzActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btAnlagenVz)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(btAnlagenVz)
+                .addContainerGap(262, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btAnlagenVzActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btAnlagenVzActionPerformed
+        // TODO add your handling code here:
+        ANLAGENVZ_GUI anlagenvz_gui = new ANLAGENVZ_GUI();
+    }//GEN-LAST:event_btAnlagenVzActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +98,6 @@ public class MainGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btAnlagenVz;
     // End of variables declaration//GEN-END:variables
 }
